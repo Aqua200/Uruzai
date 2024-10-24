523ded2803ae6dbd956c0f183d24257452a20de4
Aqua200 <chinquepapa@gmail.com>
2024-10-23 18:06:21 -0700

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -4,23 +4,23 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
     await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
 let str = `*📍 GRUPO OFICIAL*
 
-   *_〾̷̸‣⃝⃨⃛⃰⁝̵̓ᝒ̷̸͙🌸̶̩ܻᝒ̷̸꯭͙𝝡꯭𝝣꯭𝗚꯭𝗨꯭𝗠꯭𝗜꯭𝗡᭄𓆩֟֯፝𓆪𝝣⃯ᵴͦ𝛒⃨ᷫ𝛆ͨ🄲⃪⃯𝛊ᷨ𝛂⃨ͦꝆ᷽ͭ🍁⃝⃙̻⃮̋⃛⃰⁌̷̸̊͟⿻᳔̶̷̸_*
-  ┃🧸❏ ${gp4}
+   *_〾̷̸‣⃝⃨⃛⃰⁝̵̓ᝒ̷̸͙🌸̶̩ܻᝒ̷̸꯭͙𝝡꯭𝝣꯭𝗚꯭𝗨꯭𝗠꯭𝗜꯭𝗡᭄𓆩֟֯፝𓆪_*
+  ┃🧸❏ ${gp4} https://chat.whatsapp.com/B5T4rPpz5cE9pQqH4swoOB
 
-   *_ ͟͞〾⃝̵͡𝑬⃗ꭙȾ⃯𝑟𝘦𝑚⃖𝚎֟֯፝͜❀̵⃕ ̤🄷𝕖ᷨɳͦᵵͭ𝐚͢𝖏♡̵̭̭̎͟͞ ₂₀፝֟֯࣪࣪࣪₁₂_*
-┃🧸❏ https://chat.whatsapp.com/J9gyFJLbhVIJXaUZlpo8Xt
+   *_ no disponible _*
+┃🧸❏ 
    
-   *_Grupo de antojar 3.0_*
-┃🧸❏ https://chat.whatsapp.com/LJKcR8QBJgu37bVFWuhRVn
+   *_No disponible_*
+┃🧸❏ 
 
-   *_Grupo Sunlight - Team _*
-┃❤️‍🔥❏ https://chat.whatsapp.com/Fy74b6fgE9SJJpHVi6CKJY
+   *_no disponible _*
+┃❤️‍🔥❏ 
 
    *_Canal Oficial_*
-┃❤️‍🔥❏ https://whatsapp.com/channel/0029VacDy0R6hENqnTKnG820
+┃❤️‍🔥❏ próximamente 
 
-   *_Canal Sunlight - Team_*
-┃❤️‍🔥❏ https://whatsapp.com/channel/0029Vam7yUg77qVaz3sIAp0z
+   *_Canal de editor_*
+┃❤️‍🔥❏ próximamente 
 *_╰━━━━━━━━━━━━━━━━⊜_*
 `
 await conn.sendButton(m.chat, str, `͟͞ 𓆩ꪶꪾ𝘿᪶𝙞ᷨ𝙖ᷞ𝙢ͣ𝙤᪶ͨ𝙣ᷜ𝙙ꫂৎ୭࠱࠭ ͟͞\n` + wm, media, [
